c9d1baa42b52a6b0bf26a0e4f438998b1b237e3e
jcheron <myaddressmail@gmail.com>
2016-06-20 03:15:00 +0200

    composer update

--- app/config/loader.php
+++ app/config/loader.php
@@ -6,7 +6,7 @@ $loader = new \Phalcon\Loader();
  * We're a registering a set of directories taken from the configuration file
  */
 $loader->registerNamespaces(array(
-		'Ajax' => __DIR__ . '/../../vendor/jcheron/phalcon-jquery/Ajax/'
+		'Ajax' => __DIR__ . '/../vendor/phpmv/php-mv-ui/Ajax/'
 ))->register();
 $loader->registerDirs(
     array(
